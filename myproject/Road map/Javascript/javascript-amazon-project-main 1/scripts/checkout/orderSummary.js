520bf7a6aa316af0d43c090b0af10a3c45ba80f7
Newyby <Officialraydata@gmail.com>
2025-09-29 11:59:19 +0100

    test continuea

--- myproject/Road map/Javascript/javascript-amazon-project-main 1/scripts/checkout/orderSummary.js
+++ myproject/Road map/Javascript/javascript-amazon-project-main 1/scripts/checkout/orderSummary.js
@@ -34,7 +34,7 @@ export function renderOrderSummary() {
     const dateString = deliveryDate.format('dddd, MMMM D');
     
     cartSummaryHtml += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container js-cart-item-container js-cart-item-container-${matchingProduct.id}">
         <div class="delivery-date">
          Delivery date: ${dateString}
         </div>
@@ -49,7 +49,7 @@ export function renderOrderSummary() {
             <div class="product-price">
               $${formerCurrency(matchingProduct.priceCents)}
             </div>
-            <div class="product-quantity">
+            <div class="product-quantity js-product-quantity-${matchingProduct.id}">
               <span>
                 Quantity: <span class="quantity-label">${cartItem.quantity}</span>
               </span>
@@ -57,7 +57,8 @@ export function renderOrderSummary() {
               data-product-id="${matchingProduct.id}">
                 Update
               </span>
-              <span class="delete-quantity-link link-primary js-delete-link" data-product-id="${matchingProduct.id}">
+              <span class="delete-quantity-link link-primary js-delete-link js-delete-link-${matchingProduct.id}"
+              data-product-id="${matchingProduct.id}">
                 Delete
               </span>
             </div>
