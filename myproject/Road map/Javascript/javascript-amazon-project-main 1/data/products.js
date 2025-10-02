632c4cf2c69594f106ac0cee9b7d1575fdd4f38c
Newyby <Officialraydata@gmail.com>
2025-10-02 15:39:59 +0100

    use inheritance and polymerisim to add size chart

--- myproject/Road map/Javascript/javascript-amazon-project-main 1/data/products.js
+++ myproject/Road map/Javascript/javascript-amazon-project-main 1/data/products.js
@@ -38,7 +38,54 @@ getPrice(){
        
 }
 
+extraInfoHtml(){
+return '';
+  
 }
+
+}
+
+class Clothing extends Product{
+  sizeChartLink;
+  constructor(productDetails){
+  super(productDetails)
+  this.sizeChartLink = productDetails.sizeChartLink; 
+  }
+  
+  extraInfoHtml(){
+    //super.extraInfoHtml();
+    return `
+    <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+    `;
+  }
+}
+
+// Practice code
+/**const tshirt = new Clothing(
+   {
+    id: "83d4ca15-0f35-48f5-b7a3-1ea210004f2e",
+    image: "images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg",
+    name: "Adults Plain Cotton T-Shirt - 2 Pack",
+    rating: {
+      stars: 4.5,
+      count: 56
+    },
+    priceCents: 799,
+    keywords: [
+      "tshirts",
+      "apparel",
+      "mens"
+    ],
+    type: "clothing",
+    sizeChartLink: "images/clothing-size-chart.png"
+  }
+  
+);
+
+console.log( tshirt);
+console.log(tshirt.getPrice())
+
+**/
 
 /** 
  * just an example to learn class above
@@ -725,6 +772,9 @@ export const products = [
   }
 ].map(
 (productDetails)  => {
+  if(productDetails.type === 'clothing'){
+  return new Clothing (productDetails);
+  }
   return new Product(productDetails)
 }
 );
