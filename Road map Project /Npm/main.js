fd9cd806851fab8b35f07b06bb802213c5aceb1e
Newyby <Officialraydata@gmail.com>
2026-06-07 22:55:07 +0100

    npm update

--- Road map Project /Npm/main.js
+++ Road map Project /Npm/main.js
@@ -1,3 +1,6 @@
 console.log('')
 let myDate = new Date();
 console.log(myDate.toString());
+var moment = require("moment"); // require
+var myCoolDate = moment(myDate).format("LL"); 
+console.log(myCoolDate)
