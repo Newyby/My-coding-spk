e08f89b0b7e21d9c4bc9052bee0b59bed1535adb
Newyby <Officialraydata@gmail.com>
2026-06-09 20:41:31 +0100

    update script

--- Road map Project /Npm/Npm project/script.js
+++ Road map Project /Npm/Npm project/script.js
@@ -1,3 +1,8 @@
 const dayjs = require('dayjs');
 
+const timeElement =
+  document.querySelector('.current-time');
+
+timeElement.textContent =
+  dayjs().format('HH:mm:ss');
 console.log(dayjs().format());
